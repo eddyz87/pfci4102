ce5dbea3adc311e5488a2b9505422f717c89f1e7
eddyz87 <eddyz87@gmail.com>
2014-07-26 01:24:20 +0300

    added a few fixes

--- lisp-compiler.lisp
+++ lisp-compiler.lisp
@@ -1,34 +1,28 @@
 (in-package :pc)
 
 (deftype lms-instruction () '(or symbol list)
-         "symbol - label in the code
+  "symbol - label in the code
           list - (instruction &rest params)")
 
 (defun find-variable (name bindings)
   (loop for i from 0
-       for env in bindings do
-       (loop for j from 0
-            for var in env
-            when (eq var name)
-            do (return-from find-variable (list i j))))
+        for env in bindings do
+          (loop for j from 0
+                for var in env
+                when (eq var name)
+                  do (return-from find-variable (list i j))))
   (error "Can't find variable ~A" name))
 
-;; TODO: def-test
-;; PC> (FIND-VARIABLE 'a '((b c) (d a) (e f)))
-;; (1 . 1)
-;; PC> (FIND-VARIABLE 'a '((b c) (g h) (d a) (e f)))
-;; (2 . 1)
-;; PC> (FIND-VARIABLE 'a '((b c) (g h) (d a1) (e f)))
-;; error
 
 (defvar *current-program* nil)
 (defvar *current-blocks* nil)
 
-(defmacro new-block (&body body)
+(defmacro new-block (return-instruction &body body)
   (let ((label (gensym "label-name")))
     `(let ((*current-program* nil)
            (,label (gensym "label")))
        ,@body
+       (push-instruction `(,,return-instruction))
        (push (cons ,label (reverse *current-program*))
              *current-blocks*)
        ,label)))
@@ -36,20 +30,19 @@
 (defun push-instruction (instr)
   (push instr *current-program*))
 
-;; PC> (compile-lisp '((lambda (a) (+ a a)) 21))
-;; ((LD 21) (LDF #:|label1092|) (AP 1) #:|label1092| (LD 0 0) (LD 0 0) (ADD))
-
 (defun lms-compile (term bindings)
   "bindings - list of all environments"
   (optima:match term
     ((list 'if cond then else)
      (lms-compile cond bindings)
-     (let* ((then-label (new-block (lms-compile then bindings)))
-            (else-label (new-block (lms-compile else bindings))))
+     (let* ((then-label (new-block 'join (lms-compile then bindings)))
+            (else-label (new-block 'join (lms-compile else bindings))))
        (push-instruction `(sel ,then-label ,else-label))))
     
     ((list* 'lambda params body)
-     (let ((label (new-block (lms-compile `(progn ,@body) (cons params bindings)))))
+     (let ((label (new-block 'rtn (lms-compile
+                                   `(progn ,@body)
+                                   (cons params bindings)))))
        (push-instruction `(ldf ,label))))
     
     ((list* 'progn body)
@@ -68,6 +61,16 @@
        (lms-compile p bindings))
      (case function
        (+ (push-instruction `(add)))
+       (- (push-instruction `(sub)))
+       (* (push-instruction `(mul)))
+       (/ (push-instruction `(div)))
+       (= (push-instruction `(ceq)))
+       (> (push-instruction `(cgt)))
+       (>= (push-instruction `(cgte)))
+       (car (push-instruction `(car)))
+       (cdr (push-instruction `(cdr)))
+       (cons (push-instruction `(cons)))
+       (atom (push-instruction `(atom)))
        (otherwise 
         (lms-compile function bindings)
         (push-instruction `(ap ,(length params))))))
@@ -78,11 +81,14 @@
     ((guard x (numberp x))
      (push-instruction `(ld ,x)))
 
-    ))
+    )
+  *current-program*)
 
 (defun compile-lisp (term)
-  (let ((*current-program* nil))
+  (let ((*current-program* nil)
+        (*current-blocks* nil))
     (lms-compile term '((initial-state ghost-programs)))
+    (push-instruction `(rtn))
     (append
      (reverse *current-program*)
      (mapcan #'copy-list *current-blocks*))))
