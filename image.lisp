c5dbe4b308fe7237c1bebb1e1b44715bc789958d
Igor <igor.myth@gmail.com>
2014-07-26 13:13:27 +0300

    Fixes in compiler

--- image.lisp
+++ image.lisp
@@ -4,6 +4,7 @@
 (in-package :cl-user)
 (load "pacman.asd")
 (asdf::compile-system :pacman)
+(asdf::load-system :pacman)
 
 (print "Welcome to PFCI 4102 Contest's Pacman bot")
 (sb-ext:save-lisp-and-die "pacman.image" :executable t)
